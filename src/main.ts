7530c372ab90dc38999ce7632be8fdc32398909a
Fuuki Otsuka <fuki.otsuka@ysinc.co.jp>
2025-11-26 16:32:40 +0900

    feat : 別ポートで起動できるように変更

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // platform-express アダプターを使用して Nest アプリケーションを作成
+  const app = await NestFactory.create(AppModule, new ExpressAdapter());
+
+  // 別のポート (例: 3001) で起動するように変更
+  await app.listen(3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
